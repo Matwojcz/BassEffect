3945ee4f942994fecfd66c6cc5d9138e989e50aa
Matwojcz <mateuszwojczynski@icloud.com>
2026-03-26 22:04:52 +0000

    Compressor basics implemented and wired, next attack/release logic

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #include <numbers>
 #define SAMPLE_RATE (48000)
 
-
 class ToneTester
 {
 public:
@@ -16,42 +15,46 @@ public:
     };
 };
 
-
-
-struct CompressorState // change to class Compressor and refactor
+class Compressor // change to class Compressor and refactor
 {
 public:
-    // float drive = *inB++ * state->gain;
-
-    float gain = 2.0f;
+    float gain = 1.0f;
     float rms = 0.0f;
+    float tav = 0.1f;
+    float xrms = 0.0f;
+    float compThreshold = 0.3f;
+
+    float compression(float sample)
+    {
+        xrms = (1 - tav) * xrms  + tav * (sample * sample);
+        if (xrms < compThreshold)
+        {
+            gain = 1.0f;
+        }
+        else
+        {
+            gain = compThreshold / xrms;
+            std::cout << "Compressing, rms " << xrms << " gain: "<< gain << std::endl;
+        }
+        return sample * gain;
+    }
 };
 
 class Overdrive
 {
 public:
-    float gain = 40.0f;
+    float gain = 20.0f;
     float waveshaping(float inputBuffer)
     {
-
-        // float drive = *inB++ * state->gain;
-        //Waveshaping formula y = x / 1 + abs(x)
-        // float waveshaping = drive / (1 + abs(drive));
-        //Waveshaping with tanh
-        // float waveshaping = tanh(drive);
-        // normalizing signal so the volume isn't raised by raising gain
-        // *outB++ = waveshaping * (1.0f/state->gain);
-
-
         float drive = inputBuffer * gain;
-        return tanh(drive) * (1.0f/gain);
-
+        // return tanh(drive) * (1.0f/gain);
+        return tanh(drive);
     }
 
 };
 ToneTester toneTester;
 Overdrive overdrive;
-CompressorState compState;
+Compressor compressor;
 
 static int paBassOverdriveCallback( const void *inputBuffer, void *outputBuffer,
                            unsigned long framesPerBuffer,
@@ -59,16 +62,18 @@ static int paBassOverdriveCallback( const void *inputBuffer, void *outputBuffer,
                            PaStreamCallbackFlags statusFlags,
                            void *userData )
 {
-    CompressorState *state = static_cast<CompressorState*>(userData);
+    Compressor *state = static_cast<Compressor*>(userData);
 
     auto* outB = static_cast<float*>(outputBuffer);
     auto* inB = static_cast<const float*>(inputBuffer);
     for (unsigned int i = 0; i < framesPerBuffer; i++ )
     {
-
-
+        float phaser;
+        std::cout << compressor.xrms << std::endl;
         // *outB++ = overdrive.waveshaping(*inB++);
-        *outB++ = overdrive.waveshaping(toneTester.bassPhaser(SAMPLE_RATE, 80.0f));
+        // *outB++ = overdrive.waveshaping(toneTester.bassPhaser(SAMPLE_RATE, 80.0f));
+        phaser = overdrive.waveshaping(toneTester.bassPhaser(SAMPLE_RATE, 80.0f));
+        *outB++ = state->compression(phaser);
 
         // *outB++ = *inB++;
     }
@@ -146,7 +151,7 @@ int main()
                              64,
                              paNoFlag,
                              paBassOverdriveCallback,
-                             &compState)
+                             &compressor)
      );
 
         /* Start Streaming*/
